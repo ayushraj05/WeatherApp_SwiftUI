99c1fda4f16675453ccf971e04a3d7cbedcc79d1
Ayush Rajpal <ayushrajpal2021@gmail.com>
2025-06-27 22:50:40 +0530

    set up

---
+++ WeatherApp_swiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherApp_swiftUI
+//
+//  Created by Ayush Rajpal on 27/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherApp_swiftUI/WeatherApp_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_swiftUIApp.swift
+//  WeatherApp_swiftUI
+//
+//  Created by Ayush Rajpal on 27/06/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
