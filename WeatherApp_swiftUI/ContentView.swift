933c1ec2609e3b8e1ae6f19c5b809a3fecb336c3
Ayush Rajpal <ayushrajpal2021@gmail.com>
2025-06-27 23:33:31 +0530

    add initial view

--- WeatherApp_swiftUI/ContentView.swift
+++ WeatherApp_swiftUI/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(
+                gradient:Gradient(colors:[.blue,.white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Jaipur, Rajasthan")
+                    .font(.system(size: 32, weight: .medium,design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack(spacing: 5) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit )
+                        .frame(width: 180,height: 180)
+                        
+                    Text("38°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                Spacer()
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
